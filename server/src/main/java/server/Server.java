2c877c1868157048de83e53b79b787c894d08f9f
Luis Burgos <luisarturo.burgos@gmail.com>
2015-09-21 16:55:25 -0500

    [Server] Add base classes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,7 +1,27 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * Created by luisburgos on 21/09/15.
  */
 public class Server {
+
+    private static int portNumber = 2222;
+
+    public static void main(String[] args) throws IOException {
+
+        boolean listening = true;
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            while (listening) {
+                new ServerThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+
 }

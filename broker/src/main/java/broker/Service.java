7086d3215a2f5ca46852f916fd786137ccb33f77
Jose Julio Heredia Barrera <josejulio@merostudios.com>
2015-09-27 18:42:37 -0500

    [Services] Se agrego la clase services, se adapto el broker para verificar si existe un servicio

---
+++ broker/src/main/java/broker/Service.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package broker;
+
+/**
+ *
+ * @author JoseJulio
+ */
+public class Service {
+    
+    private String ip;
+    private int port;
+    private String service;
+    
+    public Service(String ip, int port, String services){
+        this.ip = ip;
+        this.port = port;
+        this.service = services;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+    
+    
+    
+}

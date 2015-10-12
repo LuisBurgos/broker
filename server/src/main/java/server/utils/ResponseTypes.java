0b540b3fc6304605dd721ee4667ed29f64c3fb78
Luis Burgos <luisarturo.burgos@gmail.com>
2015-10-12 02:52:26 -0500

    [Server] Added response types class

---
+++ server/src/main/java/server/utils/ResponseTypes.java
@@ -0,0 +1,13 @@
+package server.utils;
+
+/**
+ * Created by luisburgos on 11/10/15.
+ */
+public class ResponseTypes {
+
+    public static final int CONNECTED = 0;
+    public static final int SERVICE_FOUND = 1;
+    public static final int SERVICE_NOT_FOUND = 2;
+    public static final int DISCONNECTED = 3;
+    public static final int REQUEST_RECEIVED = 4;
+}
